547522a4b7b60ceca1cd06e2de7954bc446337cb
James Waples <jamwaffles@googlemail.com>
2012-01-30 10:49:44 +0000

    No content as of yet, Home and Contact files added

---
+++ pages/contact.php
@@ -0,0 +1,10 @@
+<h1>Contact Us</h1>
+
+<p>
+	If you wish to contact the team, you can do so using the form below. We will respond to your message as soon we can.
+</p>
+
+<p>
+	<strong>Fields in bold are required</strong>
+</p>
+
--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,21 @@
-Home.php. Hay, it works!
+<h1>Welcome to Systemetric - Team 759</h1>
+
+<div class="contentBox half">
+	Carousel here
+</div>
+
+<div class="contentBox half">
+	About us
+</div>
+
+<div class="twitter">
+	Twitter
+</div>
+
+<div class="contentBox half">
+	Latest crap from our blog
+</div>
+
+<div class="contentBox half">
+	Links to other sites
+</div>
