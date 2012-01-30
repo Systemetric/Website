a5716326ec0f62f785aeab818ad98ad4caacb46c
James Waples <jamwaffles@googlemail.com>
2012-01-30 10:44:33 +0000

    Not ready for use yet. Added LESS compilation code

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 require_once('lib/global.php');
+require_once('lib/less.php');
+
+// In debug mode, MAKE SURE we get a new CSS (from LESS) file every time by deleting the current ont
+if($conf->debug) {
+	@unlink('style.css');
+}
+
+// Compile LESS to CSS
+try {
+	lessc::ccompile('less/global.less', 'style.css');
+} catch (exception $ex) {
+	Debug::msg($ex->getMessage());
+}
 
 Page::setup();
-Page::addContent();
+
+require_once('lib/html/header.php');
+
+//Page::addContent();
+
+require_once('lib/html/footer.php');
 ?>
