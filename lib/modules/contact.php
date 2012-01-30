775afd106e747f2f2eacbb6551f1639acd8e72a3
James Waples <jamwaffles@googlemail.com>
2012-01-30 10:48:12 +0000

    Folder for modules to be put in (gallery, etc)

---
+++ lib/modules/contact.php
@@ -0,0 +1,25 @@
+<?php
+class ContactForm {
+	private $name, $email, $subject, $body, $copyToSender;
+
+	// Get all fields from $_POST and store in $this
+	function __construct() {
+
+	}
+
+	// Validate fields. Chuck your rules in here.
+	function validate() {
+
+	}
+
+	// Actually send the email. Uses data from /config.xml
+	function sendEmail() {
+
+	}
+
+	// Clears $_POST so form is blank
+	function clearForm() {
+
+	}
+}
+?>
