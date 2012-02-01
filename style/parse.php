49bba7d0b9c3785ad316ca6692a959834bc987be
James Waples <jamwaffles@gmail.com>
2012-02-01 13:03:09 +0000

    Made it work, added debug code

--- style/parse.php
+++ style/parse.php
@@ -1,9 +1,22 @@
 <?php
-require_once("lib/less.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/lib/less.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/lib/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/lib/debug.php');
+
 header("Content-type: text/css");
 
 $name = @$_GET['name'];
-lessc::ccompile("$name.less", "compiled/$name.css");
 
-echo file_get_contents("compiled/$name.css");
+// ALWAYS delete compiled CSS file if debug is on
+if($conf->debug) {
+	@unlink('compiled/' . $name . '.css');
+}
+
+try {
+	lessc::ccompile($name . '.less', 'compiled/' . $name . '.css');
+} catch(exception $e) {
+	Debug::msg('LESS compile failed with ' . $name . ': ' . $e->getMessage());
+}
+
+echo file_get_contents('compiled/' . $name . '.css');
 ?>
