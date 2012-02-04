e2ea50f50d7eda0df3aac68c0ab472821ecbb8f4
Jonathan Laver <jonathan.laver@gmail.com>
2012-02-04 18:52:39 +0000

    unlink duplicated in parse.php

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 require_once('lib/global.php');
 require_once('lib/less.php');
 
-// In debug mode, MAKE SURE we get a new CSS (from LESS) file every time by deleting the current ont
-if($conf->debug) {
-	@unlink('style.css');
-}
-
 Page::setup();
 
 require_once('lib/html/header.php');
