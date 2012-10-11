fd432b76e65d216dc919fae4c3c100a8ac548954
Nic Dart <nicholasjdart@gmail.com>
2012-10-11 15:55:29 +0100

    styled homepage with links, space for blog and carousel

--- lib/html/header.php
+++ lib/html/header.php
@@ -5,6 +5,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 		<link rel="stylesheet" href="style/global.less">
+		<link href="/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
 	</head>
 
 	<body>
@@ -47,27 +48,3 @@
 			</div>
 
 			<div id="main">
-				Main
-			</div>
-		</div>
-
-		<div id="footer">
-			<div>
-				Footer
-			</div>
-		</div>
-
-		<!-- Fetch jQuery from CDN, our JS from the server -->
-		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-
-		<?php
-		// Fetch all our own JS
-		$files = glob('javascript/*.js');
-
-		// Loop through matched files and print `<script>` tags
-		if(!empty($files))
-			foreach($files as $file)
-			echo '<script type="text/javascript" src="' . $file . '"></script>';
-		?>
-	</body>
-</html>
--- pages/home.php
+++ pages/home.php
@@ -1,21 +1,38 @@
 <h1>Welcome to Systemetric - Team 759</h1>
 
-<div class="contentBox half">
-	Carousel here
+
+<div class="contentBox genericIntro">
+	<div class="contentBox half carousel">
+		Carousel here
+	</div>
+
+	<div class="contentBox half introduction">
+		<p>Systemetric is a Robotics Team comprised of students aged 16-18 from Hills Road Sixth Form College in Cambridge, UK. We have been competing in the FIRST Robotics Competition since 2002, becoming the first British team to compete, and since then have competed every year at the competition regional in New York City. We also mentor FIRST Lego League teams in the East of England and often take part in other smaller robotics competitions.</p>
+		<p>Read more about us...</p>
+	</div>
 </div>
 
-<div class="contentBox half">
-	About us
-</div>
-
-<div class="twitter">
+<!--<div class="twitter" style="width:95% height:15px;margin:7px auto;background-color:green;">
 	Twitter
+</div> -->
+
+<div class="contentBox half layoutSplit">
+	<h3>Latest from out Blog</h3>
 </div>
 
-<div class="contentBox half">
-	Latest crap from our blog
-</div>
-
-<div class="contentBox half">
-	Links to other sites
+<div class="contentBox half layoutSplit">
+	<h3>Links</h3>
+	<p>
+		<a href="" target="_blank">
+			<img src="/images/first.png"/>
+		</a>
+		<a href="" target="_blank">
+			<img src="/images/hrsfc.png" />
+		</a>
+	</p>
+	<p>
+		<a href="" target="_blank">
+			<img src="/images/sturobo.png" />
+		</a>
+	</p>
 </div>
