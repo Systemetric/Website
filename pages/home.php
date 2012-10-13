4643c3ae4813e59ac6005473b90b4436bd2e0472
Jonathan Laver <jonathan.laver@gmail.com>
2012-10-13 20:32:25 +0100

    removed commented code

--- pages/home.php
+++ pages/home.php
@@ -37,26 +37,4 @@
 	</div>
 </div>
 
-<!--
 
-<h1>Welcome to Systemetric - Team 759</h1>
-<div class="contentBox genericIntro">
-	<div class="contentBox half carousel">
-		<img src="/images/carousel.png" />
-	</div>
-	<div class="contentBox half introduction">
-		<p>Systemetric is a Robotics Team comprised of students aged 16-18 from Hills Road Sixth Form College in Cambridge, UK. We have been competing in the FIRST Robotics Competition since 2002, becoming the first British team to compete, and since then have competed every year at the competition regional in New York City. We also mentor FIRST Lego League teams in the East of England and often take part in other smaller robotics competitions.</p>
-		<p>Read more about us...</p>
-	</div>
-</div>
-<div style="width:70%;margin:250px auto;">
-	<div class="contentBox half layoutSplit" style="float:left;">
-		<h3>Latest from out Blog</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elementum leo viverra ipsum laoreet non iaculis massa dictum. Aenean ac orci lacus, sed placerat nunc. Morbi at ipsum lectus, at sollicitudin sapien. Sed semper, urna nec consequat adipiscing, diam quam congue lacus.</p>
-	</div>	
-
-	<div class="contentBox half layoutSplit" style="float:right;">
-		
-	</div>
-</div>
--->
